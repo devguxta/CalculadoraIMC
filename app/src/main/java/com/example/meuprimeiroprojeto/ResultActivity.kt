73fecf3c3f8a4a48ff2aa5de2425d953154524df
devguxta <148403273+devguxta@users.noreply.github.com>
2023-10-18 22:03:37 -0300

    Initial commit

---
+++ app/src/main/java/com/example/meuprimeiroprojeto/ResultActivity.kt
@@ -0,0 +1,49 @@
+package com.example.meuprimeiroprojeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val tvResult = findViewById<TextView>(R.id.textView_Result)
+        val tvClassification = findViewById<TextView>(R.id.textview_Classification)
+        val result = intent.getFloatExtra("EXTRA_RESULT", 0.1f)
+
+        tvResult.text = result.toString()
+
+        /* TABELA IMC
+        Menor que 18,5          Abaixo do peso 0
+        Entre 18,5 e 24,9       Normal 0
+        Entre 25,0 e 29,9       Sobrepeso I
+        Entre 30,0 e 39,9       Obesidade II
+        Maior que 40,0          Obesidade Grave III
+         */
+
+        val classificacao =if (result < 18.5f) {
+            "Abaixo do peso "
+        } else if (result in 18.5..24.9) {
+            "Normal "
+        } else if (result in 25f..29.9f) {
+            "Sobrepeso "
+        } else if (result in 30f..39.9f) {
+            "Obesidade "
+        } else {
+            "Obesidade Grave "
+        }
+        tvClassification.text = getString(R.string.message_classification,classificacao)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
+    }
+}
+
